d33698ec02c1994c32fb6164db0808d8bf873330
Diego Conti <diego.conti@unimib.it>
2022-09-29 20:42:59 +0200

    added script to run tests with ctest

--- sh/compile.sh
+++
@@ -1,6 +0,0 @@
-FLAGS="--std=c++17 -Isource -g -Wreorder -Wsign-promo -Wchar-subscripts -Winit-self -Wparentheses -Wreturn-type -Wswitch -Wtrigraphs -Wextra -Wno-sign-compare -Wno-narrowing -Wno-attributes"
-echo compiling with CXXFLAGS=$CXXFLAGS
-mkdir bin -p
-g++ $FLAGS $CXXFLAGS source/yggdrasill.cpp -lboost_filesystem -lboost_program_options -o bin/yggdrasill
-g++ $FLAGS $CXXFLAGS source/hliðskjálf.cpp -lboost_filesystem -lboost_program_options -lncurses -pthread -o bin/hliðskjálf
-
