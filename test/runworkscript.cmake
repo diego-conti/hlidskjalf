cb42f6c7431596a26be1852024e9aec42c362ad0
Diego Conti <diego.conti@unimib.it>
2022-10-05 18:03:27 +0200

    added "" in runworkscript.cmake

--- test/runworkscript.cmake
+++ test/runworkscript.cmake
@@ -2,8 +2,10 @@ function(cat IN_FILE OUT_FILE)
   file(READ ${IN_FILE} CONTENTS)
   file(APPEND ${OUT_FILE} "${CONTENTS}")
 endfunction()
-message (COMMAND  ${CMAKE_TOP_BINARY_DIR}/hliðskjálf --script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio)
-execute_process(COMMAND  ${CMAKE_TOP_BINARY_DIR}/hliðskjálf --script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio)
+
+string (JOIN " " LOG COMMAND  "${CMAKE_TOP_BINARY_DIR}/hlidskjalf --script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio")
+message(${LOG})
+execute_process(COMMAND "${CMAKE_TOP_BINARY_DIR}/hlidskjalf --script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio")
 
 set (UNSORTED_OUTPUT ${PROJECT_BINARY_DIR}/workscript.unsorted)
 file(WRITE ${UNSORTED_OUTPUT} "")
