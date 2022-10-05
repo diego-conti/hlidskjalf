ba8e49553a41f4da050aebe9551036890633a1fc
Diego Conti <diego.conti@unimib.it>
2022-10-05 18:09:53 +0200

    removed ""

--- test/runworkscript.cmake
+++ test/runworkscript.cmake
@@ -5,7 +5,7 @@ endfunction()
 
 string (JOIN " " LOG COMMAND  "${CMAKE_TOP_BINARY_DIR}/hlidskjalf --script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio")
 message(${LOG})
-execute_process(COMMAND "${CMAKE_TOP_BINARY_DIR}/hlidskjalf" "--script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio")
+execute_process(COMMAND ${CMAKE_TOP_BINARY_DIR}/hlidskjalf --script ${PROJECT_SOURCE_DIR}/script/workscript.m --workoutput ${PROJECT_BINARY_DIR}/output --computations ${PROJECT_SOURCE_DIR}/computations/test.comp  --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workload 1 --stdio)
 
 set (UNSORTED_OUTPUT ${PROJECT_BINARY_DIR}/workscript.unsorted)
 file(WRITE ${UNSORTED_OUTPUT} "")
