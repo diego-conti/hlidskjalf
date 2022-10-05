7cc7ac1ce521315a64558193175de41ddcb2a8f4
Diego Conti <diego.conti@unimib.it>
2022-10-05 22:07:10 +0200

    added tests for timeout

---
+++ test/script/timeoutworkscript.m
@@ -0,0 +1,31 @@
+//version that always waits d seconds when processiund d;x;y
+load "../magma/hliðskjálflayer.m";
+
+if assigned printVersion then print "test version of test script"; quit; end if;
+
+ReadLine:=function(line)
+	components:=Split(line,";");
+	if #components ne 3 then error "Each line in datafile should have the form d;X;Y ", components; end if;
+	return StringToInteger(components[1]), components[2],components[3];
+end function;
+
+WriteLine:=procedure(d,X,Y,data)
+	WriteComputation(Sprint(d) cat ";" cat X cat ";" cat Y cat ";Odin;" cat data cat ";6;7;8");
+end procedure;
+
+ComputeAndWriteToStdout:=procedure(fileName)
+	file:=Open(fileName,"r");
+	line:=Gets(file);
+	while not IsEof(line) do
+		d,X,Y:=ReadLine(line);
+		starttime:=Realtime();
+		while Realtime(starttime) lt d do
+		end while;
+		WriteLine(d,X,Y,Sprint(d) cat X cat Y);
+		line:=Gets(file);
+	end while;
+end procedure;
+
+
+ComputeAndWriteToStdout(dataFile);
+quit;
