5631d0b72daedc373cc5bd27671334c4aceee477
Diego Conti <diego.conti@unimib.it>
2022-10-06 20:16:23 +0200

    fixes in cmake script

--- test/runyggdrasill.cmake
+++ test/runyggdrasill.cmake
@@ -1,3 +1,4 @@
-file(REMOVE_RECURSE db.test)
-file(MAKE_DIRECTORY db.test)
-execute_process(COMMAND ${CMAKE_BINARY_DIR}/yggdrasill --db db.test --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workoutput ${PROJECT_SOURCE_DIR}/workoutput --output ${CMAKE_BINARY_DIR}/yggdrasillomits.test)
+set (DB_DIR ${PROJECT_BINARY_DIR}/db.test)
+file(REMOVE_RECURSE ${DB_DIR})
+file(MAKE_DIRECTORY ${DB_DIR})
+execute_process(COMMAND ${CMAKE_BINARY_DIR}/yggdrasill --db ${DB_DIR} --schema ${PROJECT_SOURCE_DIR}/script/testschema.info --workoutput ${PROJECT_SOURCE_DIR}/workoutput --output ${PROJECT_BINARY_DIR}/yggdrasillomits.test)
